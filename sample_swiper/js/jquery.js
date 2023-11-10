fe9bc5f5126553f39df01156385f767b492f1f40
12327004KoeiInoue <k-inoue4274@st.kyoto-art.ac.jp>
2023-11-10 14:23:29 +0900

    Create jquery.js

---
+++ sample_swiper/js/jquery.js
@@ -0,0 +1,12 @@
+//jqueryコード
+$(function(){
+
+  $('#btn1').on('click', function() {
+      alert("konichiwa");
+      });
+  
+  $('#btn2').on('click', function() {
+      alert("クリックされました");
+      });
+    
+    });
